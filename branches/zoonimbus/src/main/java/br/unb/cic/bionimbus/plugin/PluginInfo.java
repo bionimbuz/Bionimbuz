91c325f888f8c0bc8a2b0fd1c370d12fd68b2f17
gabriel.222zt@gmail.com <gabriel.222zt@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2013-05-19 14:34:37 +0000

    Novos dados inseridos

--- branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/plugin/PluginInfo.java
+++ branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/plugin/PluginInfo.java
@@ -1,149 +1,189 @@
 package br.unb.cic.bionimbus.plugin;
 
+import br.unb.cic.bionimbus.p2p.Host;
 import java.util.List;
 
-import br.unb.cic.bionimbus.p2p.Host;
-
 public class PluginInfo implements PluginOps {
-	
-	private String id;
-	
-	private Host host;
-	
-	private long uptime;
-	
-	private long latency;
-	
-	private long timestamp;
-	
-	private Integer numCores;
-	
-	private Integer numNodes;
-	
-	private Integer numOccupied;
-	
-	private Float fsSize;
-	
-	private Float fsFreeSize;
-	
-	private List<PluginService> services;
 
-	public String getId() {
-		return id;
-	}
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public Host getHost() {
-		return host;
-	}
+    private Host host;
 
-	public void setHost(Host host) {
-		this.host = host;
-	}
+    private long uptime;
 
-	public long getUptime() {
-		return uptime;
-	}
+    private long latency;
 
-	public void setUptime(long uptime) {
-		this.uptime = uptime;
-	}
+    private long timestamp;
 
-	public long getLatency() {
-		return latency;
-	}
+    private Integer numCores;
 
-	public void setLatency(long latency) {
-		this.latency = latency;
-	}
-	
-	public long getTimestamp() {
-		return timestamp;
-	}
-	
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
-	}
+    private Integer numNodes;
 
-	public Float getFsFreeSize() {
-		return fsFreeSize;
-	}
+    private Integer numOccupied;
 
-	public void setFsFreeSize(Float fsFreeSize) {
-		this.fsFreeSize = fsFreeSize;
-	}
+    private Long ranking = new Long(0l);
 
-	public Integer getNumCores() {
-		return numCores;
-	}
+    private Float fsSize;
 
-	public void setNumCores(Integer numCores) {
-		this.numCores = numCores;
-	}
+    private Float fsFreeSize;
 
-	public Integer getNumNodes() {
-		return numNodes;
-	}
+    private Double MemoryTotal;
 
-	public void setNumNodes(Integer numNodes) {
-		this.numNodes = numNodes;
-	}
+    private Double MemoryFree;
 
-	public Float getFsSize() {
-		return fsSize;
-	}
+    private Double FrequencyCore;
 
-	public void setFsSize(Float fsSize) {
-		this.fsSize = fsSize;
-	}
-	
-	public Integer getNumOccupied() {
-		return numOccupied;
-	}
+    private List<PluginService> services;
 
-	public void setNumOccupied(Integer numOccupied) {
-		this.numOccupied = numOccupied;
-	}
+    public String getId() {
+            return id;
+    }
 
-	public List<PluginService> getServices() {
-		return services;
-	}
+    public void setId(String id) {
+            this.id = id;
+    }
 
-	public void setServices(List<PluginService> services) {
-		this.services = services;
-	}
+    public Host getHost() {
+            return host;
+    }
 
-	public PluginService getService(long serviceId) {
-		for (PluginService service : getServices())
-			if (service.getId() == serviceId)
-				return service;
-		return null;
-	}
-	
-	@Override
-	public boolean equals(Object object) {
-		if (this == object)
-			return true;
-		
-		if (!(object instanceof PluginInfo)) {
-			return false;
-		}
-		
-		PluginInfo other = (PluginInfo) object;
-		
-		return this.id.equals(other.id);
-	}
-	
-	@Override
-	public int hashCode() {
-		return id.hashCode();
-	}
-	
-	@Override
-	public String toString() {
-		return id.toString();
-	}
+    public void setHost(Host host) {
+            this.host = host;
+    }
+
+    public long getUptime() {
+            return uptime;
+    }
+
+    public void setUptime(long uptime) {
+            this.uptime = uptime;
+    }
+
+    public long getLatency() {
+            return latency;
+    }
+
+    public void setLatency(long latency) {
+            this.latency = latency;
+    }
+
+    public long getTimestamp() {
+            return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+    }
+
+    public Float getFsFreeSize() {
+        return fsFreeSize;
+    }
+
+    public void setFsFreeSize(Float fsFreeSize) {
+            this.fsFreeSize = fsFreeSize;
+    }
+
+    public Integer getNumCores() {
+            return numCores;
+    }
+
+    public void setNumCores(Integer numCores) {
+            this.numCores = numCores;
+    }
+
+    public Integer getNumNodes() {
+            return numNodes;
+    }
+
+    public void setNumNodes(Integer numNodes) {
+            this.numNodes = numNodes;
+    }
+
+    public Float getFsSize() {
+        return fsSize;
+    }
+
+    public void setFsSize(Float fsSize) {
+            this.fsSize = fsSize;
+    }
+
+    public Integer getNumOccupied() {
+            return numOccupied;
+    }
+
+    public void setNumOccupied(Integer numOccupied) {
+            this.numOccupied = numOccupied;
+    }
+
+    public List<PluginService> getServices() {
+            return services;
+    }
+
+    public void setServices(List<PluginService> services) {
+            this.services = services;
+    }
+
+    public PluginService getService(long serviceId) {
+            for (PluginService service : getServices())
+                    if (service.getId() == serviceId)
+                            return service;
+            return null;
+    }
+
+
+    public Long getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(Long ranking) {
+        this.ranking = ranking;
+    }
+
+    public Double getFrequencyCore() {
+        return FrequencyCore;
+    }
+
+    public void setFrequencyCore(Double FrequencyCore) {
+        this.FrequencyCore = FrequencyCore;
+    }
+
+    public Double getMemoryFree() {
+        return MemoryFree;
+    }
+
+    public void setMemoryFree(Double MemoryFree) {
+        this.MemoryFree = MemoryFree;
+    }
+
+    public Double getMemoryTotal() {
+        return MemoryTotal;
+    }
+
+    public void setMemoryTotal(Double MemoryTotal) {
+        this.MemoryTotal = MemoryTotal;
+    }
+        
+    @Override
+    public boolean equals(Object object) {
+            if (this == object)
+                    return true;
+
+            if (!(object instanceof PluginInfo)) {
+                    return false;
+            }
+
+            PluginInfo other = (PluginInfo) object;
+
+            return this.id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+            return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+            return id.toString();
+    }
 }
