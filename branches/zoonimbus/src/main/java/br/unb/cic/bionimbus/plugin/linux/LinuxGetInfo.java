1dcb7a282a0700eda44dbf349b9f0344e6170c7b
gabriel.222zt@gmail.com <gabriel.222zt@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2013-05-19 14:35:37 +0000

    Alteração para coleta de novos dados

--- branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/plugin/linux/LinuxGetInfo.java
+++ branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/plugin/linux/LinuxGetInfo.java
@@ -2,36 +2,69 @@ package br.unb.cic.bionimbus.plugin.linux;
 
 import br.unb.cic.bionimbus.plugin.PluginInfo;
 import br.unb.cic.bionimbus.plugin.PluginService;
+import java.io.BufferedReader;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LinuxGetInfo implements Callable<PluginInfo> {
 
 	private static final String SERVICE_DIR = "services";
 	
 	public static final String PATH = "files";
+        
+        public static final String CORES = "grep -m 1 cores /proc/cpuinfo";
+        
+        public static final String CPUMHz = "grep -m 1 MHz /proc/cpuinfo";
+        
+        public static final String MemTotal = "grep -m 1 MemTotal /proc/meminfo";
+        
+        public static final String MemFree = "grep -m 1 MemFree /proc/meminfo";
+        
 
 	private final PluginInfo pluginInfo = new PluginInfo();
 
 	@Override
 	public PluginInfo call() throws Exception {
 		getCpuInfo();
+                getMemoryInfo();
 		getDiskInfo();
 		getServices();
 
-		return pluginInfo;
+ 		return pluginInfo;
 	}
-
+        /**
+         * Obtem as informações do processador do recurso e realiza o setter dessa informações na classe pluginInfo.
+         * Número de cores, frequencia do processador(GHz) e quantidade de cores ocupados.
+         */
 	private void getCpuInfo() {
-		pluginInfo.setNumCores(Runtime.getRuntime().availableProcessors());
-		pluginInfo.setNumNodes(1);
-		pluginInfo.setNumOccupied(0);
+            pluginInfo.setNumCores(Runtime.getRuntime().availableProcessors());
+            pluginInfo.setNumNodes(1);
+            pluginInfo.setNumOccupied(0);
+            String cpuInfo =  execCommand(CPUMHz);    
+            pluginInfo.setFrequencyCore((new Double(cpuInfo.substring(cpuInfo.indexOf(":") + 1, cpuInfo.length()).trim()))/1000);
+ 	}
+        /**
+         * Obtem as informações da memória RAM do recurso e realiza o setter dessa informações na classe pluginInfo.
+         * Memória total e memória livre em MegaBytes.
+         */
+        private void getMemoryInfo() {
+            String mem = execCommand(MemTotal);
+            pluginInfo.setMemoryTotal((new Double(mem.substring(mem.indexOf(":") + 1, mem.length()-2).trim())/1024));
+            mem = execCommand(MemFree);
+            pluginInfo.setMemoryFree((new Double(mem.substring(mem.indexOf(":") + 1, mem.length()-2).trim())/1024));
 	}
-
+        /**
+         * Obtem as informações do espaço e, disco do recurso e realiza o setter dessa informações na classe pluginInfo.
+         * Espaço total e espaço livre.
+         */
 	private void getDiskInfo() {
 		File path = new File(PATH);
 		for (File root : File.listRoots()) {
@@ -60,4 +93,25 @@ public class LinuxGetInfo implements Callable<PluginInfo> {
 		pluginInfo.setServices(list);
 	}
 
+        /**
+         * Retorna os valores da execução do comando informado pelo parâmetro
+         * @param Command comando a ser executado
+         * @return string resultado da execução
+         */
+        private String execCommand(String Command){
+            String line = null;
+            InputStreamReader read; 
+            try {
+                
+                read = new InputStreamReader(Runtime.getRuntime().exec(Command).getInputStream());
+                BufferedReader buffer  = new BufferedReader(read);
+                
+                line =  buffer.readLine();
+                
+            } catch (IOException ex) {
+                Logger.getLogger(LinuxGetInfo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            return line;
+        }
 }
