46c7d5aecb7beaac28fcf7e41ff82fcbec3b4a73
gabriel.222zt@gmail.com <gabriel.222zt@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2013-06-03 02:47:22 +0000

    implementação do comando start

--- branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/avro/rpc/BioProtoImpl.java
+++ branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/avro/rpc/BioProtoImpl.java
@@ -2,6 +2,8 @@ package br.unb.cic.bionimbus.avro.rpc;
 
 import br.unb.cic.bionimbus.avro.gen.BioProto;
 import br.unb.cic.bionimbus.avro.gen.JobCancel;
+import br.unb.cic.bionimbus.client.JobInfo;
+import br.unb.cic.bionimbus.services.ZooKeeperService;
 import br.unb.cic.bionimbus.services.discovery.DiscoveryService;
 import br.unb.cic.bionimbus.services.sched.SchedService;
 import br.unb.cic.bionimbus.services.storage.StorageService;
@@ -9,22 +11,26 @@ import com.google.inject.Inject;
 import org.apache.avro.AvroRemoteException;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.*;
+import java.util.Collection;
 
 public class BioProtoImpl implements BioProto {
 
     private final DiscoveryService discoveryService;
     private final StorageService storageService;
     private final SchedService schedService;
+    private final ZooKeeperService zkService;
 
     @Inject
-    public BioProtoImpl(DiscoveryService discoveryService, StorageService storageService, SchedService schedService) {
+    public BioProtoImpl(DiscoveryService discoveryService, StorageService storageService, SchedService schedService, ZooKeeperService zkservice) {
         this.discoveryService = discoveryService;
         this.storageService = storageService;
         this.schedService = schedService;
+        this.zkService =  zkservice;
     }
 
     public boolean ping() throws AvroRemoteException {
@@ -45,8 +51,16 @@ public class BioProtoImpl implements BioProto {
 
     @Override
     public String startJob(String jobID) throws AvroRemoteException {
-        // TODO: call schedService
-        return "OK";
+
+        JobInfo job = new JobInfo();
+        job.setId(null);
+        job.setServiceId(Long.parseLong(jobID));
+
+        ArrayList<JobInfo> jobList = new ArrayList<JobInfo>();
+        jobList.add(job);
+        
+        schedService.getPolicy().schedule(jobList, zkService);
+        return "Job Executado";
     }
 
     @Override
