fadf15ecddec9d3026287d12b9eca3e3a51444cb
gabriel.222zt@gmail.com <gabriel.222zt@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2013-06-05 20:01:02 +0000

    criação metodo para retornar peers

--- branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/services/AbstractBioService.java
+++ branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/services/AbstractBioService.java
@@ -6,6 +6,15 @@ package br.unb.cic.bionimbus.services;
 
 
 import br.unb.cic.bionimbus.p2p.P2PListener;
+import br.unb.cic.bionimbus.plugin.PluginInfo;
+import br.unb.cic.bionimbus.services.discovery.DiscoveryService;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.zookeeper.KeeperException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  * @author biocloud1
@@ -13,5 +22,35 @@ import br.unb.cic.bionimbus.p2p.P2PListener;
 public abstract class AbstractBioService implements Service, P2PListener, Runnable {
 
     protected ZooKeeperService zkService;
+    private static final String ROOT_PEER = "/peers";
+    private static final String SEPARATOR = "/";
+    
+    public List<PluginInfo> getPeers(){
+        List<String> children;
+        List<PluginInfo> listPlugin= new ArrayList<PluginInfo>();
+
+        try {
+
+            children = zkService.getChildren(ROOT_PEER, null);
+
+            for (String child : children) {
+                String childStr = zkService.getData(ROOT_PEER +SEPARATOR+ child, null);
+                ObjectMapper mapper = new ObjectMapper();
+                PluginInfo myInfo = mapper.readValue(childStr, PluginInfo.class);
+                listPlugin.add(myInfo);
+                    
+            }
+        } catch (KeeperException ex) {
+            Logger.getLogger(DiscoveryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(DiscoveryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(DiscoveryService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        return listPlugin;
+    }
+    
 
 }
