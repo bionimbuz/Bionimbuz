6bb1de9e4194c09b247ff4b3af1678851d779cbb
gabriel.222zt@gmail.com <gabriel.222zt@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2013-06-10 12:39:24 +0000

    atualizações no schedService e adicionando classe implementação Watch para o zookeeper (2)

--- branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/plugin/PluginTaskState.java
+++ branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/plugin/PluginTaskState.java
@@ -1,9 +1,14 @@
 package br.unb.cic.bionimbus.plugin;
 
 public enum PluginTaskState {
+    //job aguardando escalonamento
     PENDING,
+    //job escalonado aguardando execução
     WAITING,
+    //job atualmente sendo executado
     RUNNING,
+    //job executado
     DONE,
+    //job cancelado
     CANCELLED,
 }
