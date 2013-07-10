60dbd4ce8cf31248594a16f76d6099de25ee7307
gabriel222zt@gmail.com <gabriel222zt@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2013-07-10 18:33:11 +0000

    adicionando comando de status

--- branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/client/shell/commands/JobStatus.java
+++ branches/zoonimbus/src/main/java/br/unb/cic/bionimbus/client/shell/commands/JobStatus.java
@@ -1,14 +1,25 @@
 package br.unb.cic.bionimbus.client.shell.commands;
 
+import br.unb.cic.bionimbus.avro.gen.BioProto;
 import br.unb.cic.bionimbus.client.shell.Command;
+import br.unb.cic.bionimbus.client.shell.SimpleShell;
 
 public class JobStatus implements Command {
 
     public static final String NAME = "status";
 
+    private final SimpleShell shell;
+
+    public JobStatus(SimpleShell shell) {
+        this.shell = shell;
+    }
+    
     @Override
     public String execute(String... params) throws Exception {
-        return "not implemented yet";
+        
+        BioProto rpc = shell.getProxy();
+        
+        return rpc.statusJob(params[0]);
     }
 
     @Override
