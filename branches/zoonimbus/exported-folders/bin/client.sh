53590c0b4c57d2ac366297ddde4ea94aa6ead41f
gabriel222zt@gmail.com <gabriel222zt@gmail.com@5d6e329d-31bd-10ec-1e50-14ab84c78458>
2013-07-11 15:15:28 +0000

    Atualizando configurações

--- branches/zoonimbus/exported-folders/bin/client.sh
+++ branches/zoonimbus/exported-folders/bin/client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -server -Xms512m -Xmx1G -cp bionimbus-1.0-SNAPSHOT-bundle.jar br.unb.cic.bionimbus.client.shell.SimpleShell
+java -server -Xms256m -Xmx512m -cp bionimbus-1.0-SNAPSHOT-bundle.jar br.unb.cic.bionimbus.client.shell.SimpleShell
