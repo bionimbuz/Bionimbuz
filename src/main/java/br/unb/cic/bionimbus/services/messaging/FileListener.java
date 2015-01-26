c9b8d3e164c16bbb2a7d8f91b653f8ee23a6a15a
Willian Junior <guns945@gmail.com>
2015-01-26 16:28:57 -0200

    Classes Removidas: MessageService, MessageServiceServer, MessageServiceClient, MessageServiceServerHandler, MessageServiceClientHandler, MessageServiceServerHttpHandler, MessageServiceFileClientHandler e FileListener. Classes que foram afetadas/classes com erros: P2PServer, MessageServiceClientPipelineFactory, MessageServiceFileClientPipelineFactory e MessageServiceServerDecoder

--- src/main/java/br/unb/cic/bionimbus/services/messaging/FileListener.java
+++ src/main/java/br/unb/cic/bionimbus/services/messaging/FileListener.java
@@ -1,10 +1,7 @@
 package br.unb.cic.bionimbus.services.messaging;
 
-import java.io.File;
-import java.util.Map;
-
 public interface FileListener {
 
-    void onFileReceived(File file, Map<String, String> parameters);
+    //void onFileReceived(File file, Map<String, String> parameters);
 
 }
