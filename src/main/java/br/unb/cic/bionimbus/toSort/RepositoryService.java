3aa70c194677763aa7a6e8788b639bc3f186f390
Willian <guns945@gmail.com>
2015-06-08 17:27:32 -0300

    funções preliminares criadas. ainda é necessario criar outras classes entidades. é possivel que algumas dessas funções sejam removidas no futuro

--- src/main/java/br/unb/cic/bionimbus/toSort/RepositoryService.java
+++ src/main/java/br/unb/cic/bionimbus/toSort/RepositoryService.java
@@ -5,11 +5,45 @@
  */
 package br.unb.cic.bionimbus.toSort;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author will
  * O nome da classe é temporario, assim como sua localização
+ * 
+ * Dados disponiveis atraves de metodos get
  */
 public class RepositoryService {
+    
+    // TODO: deve haver uma classe basica contendo as informações de instancias
+    // pergunta: qual é a nomeclatura para uma instancia de infra que não foi ativada e 
+    //    qual é a nomeclatura para uma instancia ativa (executando algo)
+    public List<Instances> getInstancesList() {
+        // garante que a lista retornada pode ser a referencia atual, não precisando ser uma copia
+        return Collections.unmodifiableList(null);
+    }
+    
+    public float getInstanceCost(int instId) {
+        return 0;
+    }
+    
+    // TODO: criar uma classe para arvores (arvores em java parecem ser demasiadamente/desnecessariamente complicadas)
+    public TaskNode getDependencyTree(int pipelineId) {
+        return null;
+    }
+    
+    // TODO: criar classe que representa um arco na rede (i.e. entre duas instancia ativa)
+    // retorna uma lista de arcos e os valores de throughput e latencia dos mesmos
+    public List<Latency> getNetworkList () {
+        return Collections.unmodifiableList(null);
+    }
+    
+    // retorna um dos elementos da lista de historicos
+    // lista atualizada preguiçosamente
+    public List<Long> getTaskHistory (int taskId) {
+        return Collections.unmodifiableList(null);
+    }
     
 }
