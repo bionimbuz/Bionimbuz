799219d6341d99cfdf5e4f90abaa77dd327e2ab7
Diego Azevedo <diego.optimist.2840@gmail.com>
2015-06-22 20:14:57 -0300

    Tamanho Mínimo para compressão

--- src/main/java/br/unb/cic/bionimbus/utils/Put.java
+++ src/main/java/br/unb/cic/bionimbus/utils/Put.java
@@ -1,5 +1,6 @@
 package br.unb.cic.bionimbus.utils;
 
+import java.io.File;
 import java.io.IOException;
 
 import br.unb.cic.bionimbus.services.storage.bandwidth.BandwidthCalculator;
@@ -14,76 +15,95 @@ import com.jcraft.jsch.SftpException;
 
 /**
  * Classe com os metodos para a realização de um upload na federação
+ * 
  * @author Deric
  */
 public class Put {
 
-    private JSch jsch = new JSch();
-    private Session session = null;
-    private String address;
-    private String USER = "zoonimbus";
-    private String PASSW = "Zoonimbus1";
-    private int PORT = 22;
-    private Channel channel;
-    private String path;
+	private static final Long MIN_SIZE_FOR_COMPRESSION = 10 * 1024 * 1024L;
+	private JSch jsch = new JSch();
+	private Session session = null;
+	private String address;
+	private String USER = "zoonimbus";
+	private String PASSW = "Zoonimbus1";
+	private int PORT = 22;
+	private Channel channel;
+	private String path;
 
-    public Put(String address, String path) {
-        this.address = address;
-        this.path = path;
-    }
+	public Put(String address, String path) {
+		this.address = address;
+		this.path = path;
+	}
 
-    public Put() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	public Put() {
+		throw new UnsupportedOperationException("Not supported yet."); // To
+																		// change
+																		// body
+																		// of
+																		// generated
+																		// methods,
+																		// choose
+																		// Tools
+																		// |
+																		// Templates.
+	}
 
-    /**
-     * Método que realiza a conexão entre o cliente e o servidor ou entre servidores,
-     * para upar um arquivo em um peer.
-     * @return - true se o upload foi realizado com sucesso, false caso contrário
-     * @throws JSchException
-     * @throws SftpException
-     */
-    public boolean startSession() throws JSchException, SftpException {
-        String pathDest = "/home/zoonimbus/zoonimbusProject/data-folder/";
-        try {
+	/**
+	 * Método que realiza a conexão entre o cliente e o servidor ou entre
+	 * servidores, para upar um arquivo em um peer.
+	 * 
+	 * @return - true se o upload foi realizado com sucesso, false caso
+	 *         contrário
+	 * @throws JSchException
+	 * @throws SftpException
+	 */
+	public boolean startSession() throws JSchException, SftpException {
+		String pathDest = "/home/zoonimbus/zoonimbusProject/data-folder/";
+		try {
 
-            session = jsch.getSession(USER, address, PORT);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.setPassword(PASSW);
-            session.connect();
-        } catch (JSchException e) {
-            return false;
-        }
+			session = jsch.getSession(USER, address, PORT);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.setPassword(PASSW);
+			session.connect();
+		} catch (JSchException e) {
+			return false;
+		}
 
-        try {
-            this.channel = session.openChannel("sftp");
-            channel.connect();
-            ChannelSftp sftpChannel = (ChannelSftp) channel;
-            
-            String toBeSent;
-            try {
-            	System.out.println("\n Compressing file.....\n\n\n");
-				toBeSent = CompressPolicy.verifyAndCompress(path, BandwidthCalculator.linkSpeed(address));
-			} catch (IOException e) {
-				toBeSent = path;
+		try {
+			this.channel = session.openChannel("sftp");
+			channel.connect();
+			ChannelSftp sftpChannel = (ChannelSftp) channel;
+
+			String toBeSent = path;
+			if (new File(toBeSent).getTotalSpace() >= MIN_SIZE_FOR_COMPRESSION) {
+				try {
+
+					System.out.println("\n Compressing file.....\n\n\n");
+					toBeSent = CompressPolicy.verifyAndCompress(path,
+							BandwidthCalculator.linkSpeed(address));
+				} catch (IOException e) {
+					toBeSent = path;
+				}
 			}
-            /*
-             * Sem setar nenhuma permissao o arquivo chega trancado no destino, sendo acessado apenas pelo root,
-             * portanto preferi dar um 777 antes de enviar o arquivo para que chegue livre ao destino.
-             * Por questões de segurança, talvez isso deva ser repensado futuramente.
-             */
-            //sftpChannel.chmod(777, path);
-            System.out.println("\n Uploading file.....\n\n\n");
-                sftpChannel.put(toBeSent, pathDest);
-                sftpChannel.exit();
-                session.disconnect();
-                
-            CompressPolicy.deleteIfCompressed(toBeSent);
+			/*
+			 * Sem setar nenhuma permissao o arquivo chega trancado no destino,
+			 * sendo acessado apenas pelo root, portanto preferi dar um 777
+			 * antes de enviar o arquivo para que chegue livre ao destino. Por
+			 * questões de segurança, talvez isso deva ser repensado
+			 * futuramente.
+			 */
+			// sftpChannel.chmod(777, path);
+			System.out.println("\n Uploading file.....\n\n\n");
+			sftpChannel.put(toBeSent, pathDest);
+			sftpChannel.exit();
+			session.disconnect();
 
-        } catch (JSchException a) {
-            return false;
-        }
-        return true;
+			CompressPolicy.deleteIfCompressed(toBeSent);
 
-    }
+		} catch (JSchException a) {
+			return false;
+		}
+		return true;
+
+	}
 }
