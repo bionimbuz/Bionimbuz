8f735e604406a2afecf44a0cb07dd975d0cba4e7
Diego Rodrigues Azevedo <diego@guiadenos.com.br>
2015-09-10 17:54:12 -0300

    corrigindo package

--- src/main/java/br/unb/cic/bionimbus/services/storage/policy/StoragePolicy.java
+++ src/main/java/br/unb/cic/bionimbus/services/storage/policy/StoragePolicy.java
@@ -2,7 +2,7 @@
 * To change this template, choose Tools | Templates
 * and open the template in the editor.
 */
-package br.unb.cic.bionimbus.services.storage;
+package br.unb.cic.bionimbus.services.storage.policy;
 
 import br.unb.cic.bionimbus.avro.gen.NodeInfo;
 import br.unb.cic.bionimbus.plugin.PluginInfo;
