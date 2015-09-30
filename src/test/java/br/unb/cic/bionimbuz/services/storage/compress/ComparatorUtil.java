87432cd3e416b44b2f8a9023240854206749de2a
Diego Azevedo <diego@guiadenos.com.br>
2015-09-30 11:38:26 -0300

    Improving test confiability

--- src/test/java/br/unb/cic/bionimbuz/services/storage/compress/ComparatorUtil.java
+++ src/test/java/br/unb/cic/bionimbuz/services/storage/compress/ComparatorUtil.java
@@ -14,18 +14,8 @@ public class ComparatorUtil {
 		BufferedReader reader1 = new BufferedReader(new FileReader(f1));
 		BufferedReader reader2 = new BufferedReader(new FileReader(f2));
 		
-		String linha1 = reader1.readLine();
-		String linha2 = reader2.readLine();
+		return IOUtils.contentEquals(reader1, reader2);
 		
-		System.out.println("Linha1: " + linha1);
-		System.out.println("Linha2: " + linha2);
-		System.out.println("Tamanho: " + new File(f2).getTotalSpace());
-		System.out.println("Tamanho2: " + (int) new File(f2).getTotalSpace());
-		
-		IOUtils.closeQuietly(reader1);
-		IOUtils.closeQuietly(reader2);
-		
-		return linha1.equals(linha2);
 	}
 	
 }
