f1bd422e677be20a673b018bc628d8f07d3c9786
vramos91 <vinicius.ramos91@gmail.com>
2015-12-05 15:30:38 -0200

    Trying to commit changes

--- src/main/java/br/unb/cic/bionimbus/rest/resource/BaseResource.java
+++ src/main/java/br/unb/cic/bionimbus/rest/resource/BaseResource.java
@@ -3,6 +3,10 @@ package br.unb.cic.bionimbus.rest.resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Base resource for other REST resources
+ * @author Vinicius
+ */
 public abstract class BaseResource {
     protected static final Logger LOGGER = LoggerFactory.getLogger(BaseResource.class);
     
