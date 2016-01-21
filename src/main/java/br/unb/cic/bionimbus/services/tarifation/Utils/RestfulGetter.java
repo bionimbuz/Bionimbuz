733ab7dc3819add8cee9924df433245cc5baff92
Gabriel Fritz Sluzala <bielfritz@gmail.com>
2016-01-21 15:00:12 -0400

    Creation of interface RestfulGetter responsible for encapsulate Get behaviors

---
+++ src/main/java/br/unb/cic/bionimbus/services/tarifation/Utils/RestfulGetter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unb.cic.bionimbus.services.tarifation.Utils;
+
+/**
+ *
+ * @author gabriel
+ */
+public interface RestfulGetter {
+
+    /**
+     *
+     * @param server - Server where the GET operation will be realized.
+     * @param address - The complement address where the operation will be
+     * realized.
+     * @return - the result entity of the GET operation, in String format.
+     */
+    public String get(String server, String address);
+}
