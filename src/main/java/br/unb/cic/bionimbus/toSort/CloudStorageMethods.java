3483e1804f95df46bd682992ef20e85acc5230a3
Lucas <lucasbaile14@hotmail.com>
2016-01-27 16:31:28 -0200

    amazon authentication working

--- src/main/java/br/unb/cic/bionimbus/toSort/CloudStorageMethods.java
+++ src/main/java/br/unb/cic/bionimbus/toSort/CloudStorageMethods.java
@@ -5,9 +5,26 @@
  */
 package br.unb.cic.bionimbus.toSort;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.*;
-import java.util.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+
 
 /**
  *
@@ -15,17 +32,21 @@ import java.util.*;
  */
 public class CloudStorageMethods {
 
-    public static String JSONPATH = "/home/baile/TESTE-BIO/cred.json";
-
     public enum StorageProvider {
 
         AMAZON,
         GOOGLE
     }
+    
+    public static String JSONPATH = "/home/baile/Dropbox/cred.json";
+    public static String AMAZONKEYPATH = "/home/baile/Dropbox/accesskey.txt";
+    
+    private static AmazonS3 s3client;
 
     public static void ExecCommand (String command) {
         
         try {
+            
             Runtime rt = Runtime.getRuntime();
             Process proc = rt.exec(command);
             System.out.println("\nRunning command: " + command);
@@ -33,10 +54,13 @@ public class CloudStorageMethods {
             InputStreamReader isr = new InputStreamReader(stderr);
             BufferedReader br = new BufferedReader(isr);
             String line = null;
+            
             while ( (line = br.readLine()) != null)
                 System.out.println("[command] " + line);
+            
             int exitVal = proc.waitFor();
             System.out.println("[command] Process exitValue: " + exitVal);
+            
         } catch (Throwable t) {
             t.printStackTrace();
         }
@@ -45,6 +69,21 @@ public class CloudStorageMethods {
     public static void StorageAuth(StorageProvider sp) {
         switch (sp) {
             case AMAZON: {
+                try {
+                    
+                    byte[] encoded = Files.readAllBytes(Paths.get(AMAZONKEYPATH));
+                    String fileContent = new String(encoded, Charset.defaultCharset());
+                    System.out.println("string: " + fileContent);
+                    String accessKeyID, accessKey;
+                    int delimiter = fileContent.indexOf(':');
+                    accessKeyID = fileContent.substring(0,delimiter);
+                    accessKey = fileContent.substring(delimiter+1);
+                    AWSCredentials credentials = new BasicAWSCredentials(accessKeyID, accessKey);
+                    s3client = new AmazonS3Client(credentials);
+                
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
                 break;
             }
             case GOOGLE: {
@@ -61,6 +100,14 @@ public class CloudStorageMethods {
     public static void StorageUploadFile(StorageProvider sp, String bucketName, String localPath, String bucketPath, String fileName) {
         switch (sp) {
             case AMAZON: {
+                try {
+                    
+                    File file = new File (localPath + "/" + fileName);
+                    s3client.putObject(new PutObjectRequest(bucketName+bucketPath, fileName , file));
+                    
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
                 break;
             }
             case GOOGLE: {
@@ -122,9 +169,11 @@ public class CloudStorageMethods {
     public static void main(String[] args) {
         System.out.println("Iniciando Teste.\n\n");
         
-        StorageAuth(StorageProvider.GOOGLE);
+        StorageAuth(StorageProvider.AMAZON);
         
-        StorageUploadFile(StorageProvider.GOOGLE, "bionimbuzteste", "/home/baile/TESTE-BIO", "", "mclovin.png");   
+        StorageUploadFile(StorageProvider.AMAZON, "testusastandard", "/home/baile/TESTE-BIO", "/test", "mclovin.png");   
+        
+        //StorageUmount("/home/baile/TESTE-BIO/mybucket");
         
         System.out.println("\n\nTeste Realizado.");
     }
