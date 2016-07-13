6d01fba4cd3ccc7212cbb9bbc2e0ae3b05780c62
Gabriel Fritz Sluzala <bielfritz@gmail.com>
2016-07-13 20:11:10 -0300

    Creating new class Index, adding functionalities for services infos URLs

--- src/main/java/br/unb/cic/bionimbus/services/tarifation/Amazon/Index.java
+++ src/main/java/br/unb/cic/bionimbus/services/tarifation/Amazon/Index.java
@@ -31,12 +31,11 @@ public class Index {
     private String amazonCloudFront;
     private String awskms;
     private String amazonVPC;
-    private final RestfulGetter getter = new PricingGet();
     
     public Index(String server,String index) throws JSONException, IOException {
         JSONObject amazonServicesURLs = JsonReader.readJsonFromUrl("https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json");
-        //String amazonServicesURLs = this.getter.get(server, index);
-        System.out.println(amazonServicesURLs.toString());
-        //JSONObject amazonURLs = new JSONObject(amazonServicesURLs);
+        amazonServicesURLs = amazonServicesURLs.getJSONObject("offers");
+        System.out.println(amazonServicesURLs.toString(4));
+        
     }
 }
