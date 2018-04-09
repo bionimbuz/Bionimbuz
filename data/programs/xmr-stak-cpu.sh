122f34de5b6ba00e086aba6d41d6971ccd6e7189
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2018-04-09 19:31:34 -0300

    Adicionando suporte ao minerador no BioNimbuZ

---
+++ data/programs/xmr-stak-cpu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./xmr-stak --noAMD --noNVIDIA --currency aeon -o pool.aeon.hashvault.pro:3333 -u WmteSzxeCqd2eft5SEqbbQNLxH5feTTZc6KUb3EDv4GCcSrXxh8SxBXPWHipctgrRjWnNMUuFS5BwKLkcTterk7s21qHnEbnf -p work:anders1232@mail.com --hash-count $1
