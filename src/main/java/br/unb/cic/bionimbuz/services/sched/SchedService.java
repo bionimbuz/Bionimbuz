a8544e3af4ed98b73f7fd0e84ed2e17ff3b8624a
Francisco Anderson <anders1232@mail.com>
2018-05-26 17:29:01 -0300

    Alterado escalnador utilizado, para utilizar o escalonador desenvolvido

--- src/main/java/br/unb/cic/bionimbuz/services/sched/SchedService.java
+++ src/main/java/br/unb/cic/bionimbuz/services/sched/SchedService.java
@@ -92,8 +92,8 @@ public class SchedService extends AbstractBioService implements Runnable {
     private RpcClient rpcClient;
 
     // change this to select scheduling policy
-    private final SchedPolicy.Policy policy = SchedPolicy.Policy.C99SUPERCOLIDER;
-//    private final SchedPolicy.Policy policy = SchedPolicy.Policy.SIMPLE_RATING_SCHED;
+//    private final SchedPolicy.Policy policy = SchedPolicy.Policy.C99SUPERCOLIDER;
+    private final SchedPolicy.Policy policy = SchedPolicy.Policy.SIMPLE_RATING_SCHED;
     private String idPlugin;
 
     private LinuxPlugin myLinuxPlugin;
