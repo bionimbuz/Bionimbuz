e5f7092cf654d3fde3e416e1024a8bb6c5ff8a92
Francisco Anderson <anders1232@mail.com>
2018-06-30 02:15:18 -0300

    Atualizado versão do minerador

--- data/programs/xmr-stak.sh
+++ data/programs/xmr-stak.sh
@@ -2,5 +2,9 @@
 readonly ARQUIVO_SAIDA=ResultadosMinerador.log
 
 date --rfc-3339=ns >> $ARQUIVO_SAIDA
+lscpu >> $ARQUIVO_SAIDA
+lspci >> $ARQUIVO_SAIDA
+echo XMR-STAK CHAMADO
+echo XMR-STAK CHAMADO >> $ARQUIVO_SAIDA
 /usr/bin/time -vpao $ARQUIVO_SAIDA ./xmr-stak --currency aeon -o pool.aeon.hashvault.pro:3333 -u WmteSzxeCqd2eft5SEqbbQNLxH5feTTZc6KUb3EDv4GCcSrXxh8SxBXPWHipctgrRjWnNMUuFS5BwKLkcTterk7s21qHnEbnf -p work:anders1232@mail.com $@
 date --rfc-3339=ns >> $ARQUIVO_SAIDA
