895a3a1e9e2b3ba3ebcccb911673bde8714fb6d5
Francisco Anderson <anders1232@mail.com>
2018-06-30 18:27:35 -0300

    O script de lançamento do minerador captura mais dados da máquina que vai executar, para colocar no relatório dos testes.

--- data/programs/xmr-stak.sh
+++ data/programs/xmr-stak.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 readonly ARQUIVO_SAIDA=ResultadosMinerador.log
 
-date --rfc-3339=ns >> $ARQUIVO_SAIDA
+echo ------------EXECUCAO_INICIADA------------ >> $ARQUIVO_SAIDA
+echo ------------LSCPU------------ >> $ARQUIVO_SAIDA
 lscpu >> $ARQUIVO_SAIDA
+echo ------------LSPCI------------ >> $ARQUIVO_SAIDA
 lspci >> $ARQUIVO_SAIDA
-echo XMR-STAK CHAMADO
+echo ------------LSHW----------- >> $ARQUIVO_SAIDA
+lshw >> $ARQUIVO_SAIDA
+echo ------------XMR-STAK_CHAMADO----------- >> $ARQUIVO_SAIDA
 echo XMR-STAK CHAMADO >> $ARQUIVO_SAIDA
-/usr/bin/time -vpao $ARQUIVO_SAIDA ./xmr-stak --currency aeon -o pool.aeon.hashvault.pro:3333 -u WmteSzxeCqd2eft5SEqbbQNLxH5feTTZc6KUb3EDv4GCcSrXxh8SxBXPWHipctgrRjWnNMUuFS5BwKLkcTterk7s21qHnEbnf -p work:anders1232@mail.com $@
 date --rfc-3339=ns >> $ARQUIVO_SAIDA
+/usr/bin/time -vpao $ARQUIVO_SAIDA ./xmr-stak --currency aeon7 -o pool.aeon.hashvault.pro:3333 -u WmteSzxeCqd2eft5SEqbbQNLxH5feTTZc6KUb3EDv4GCcSrXxh8SxBXPWHipctgrRjWnNMUuFS5BwKLkcTterk7s21qHnEbnf -p work:anders1232@mail.com $@
+date --rfc-3339=ns >> $ARQUIVO_SAIDA
+echo ------------EXECUCAO_TERMINADA------------ >> $ARQUIVO_SAIDA
